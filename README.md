bc4b82b2053ae1641a784eb70c133df0b138360a
itamar reiter <itamarreiter@gmail.com>
2022-02-06 21:40:22 +0200

    copy content

---
+++ README.md
@@ -0,0 +1,30 @@
+# Project 4: Around The U.S.
+
+### Overview
+
+* Description
+* Figma
+* Images
+* Tech
+* Link 
+
+
+**Description**
+The project describes an author journey at the U.S through site's images that has been visited by him. the project allowing the user editing the profile, add and delete different images, and wath them on popup-mode.
+
+**Figma**
+
+* [Link to the project in Figma](https://www.figma.com/file/m79HxYeZpOXRw0Tz2eZGOV/Sprint-5%3A-Around-The-U.S.-%7C-desktop-%2B-mobile?node-id=1%3A398)
+
+**Images**
+
+images are from around the U.S, and they are at the images folder
+
+**Tech**
+
+Tech that were used: We used grid and flexbox displays for items-location, and media queries for the project's-resnposivity. We used BEM structure for organizing the files.
+we used JavaScript functions such as addEventListener for managing the profile's name, the images etc.
+
+**Link**
+
+[link](https://itamar-reiter.github.io/web_project_4/) for the project on gitHub.
---
+++ scripts/index.js
@@ -0,0 +1,108 @@
+const profile = document.querySelector(".profile");
+const profileName = profile.querySelector(".profile__name");
+const profileAboutMe = profile.querySelector(".profile__about-me");
+const editButton = profile.querySelector("#editButton");
+const addButton = profile.querySelector("#addButton");
+const placesContainer = document.querySelector(".grid-elements");
+const popupEditProfile = document.querySelector(".popup_type_edit-profile");
+const popupEditProfileCloseButton = popupEditProfile.querySelector(
+  ".popup__close-icon_type_form"
+);
+const popupFormEditProfile = document.querySelector(
+  ".popup__form_type_edit-profile"
+);
+const nameInput = document.querySelector(".popup__input_type_name");
+const aboutMeInput = document.querySelector(".popup__input_type_about-me");
+const popupAddPhoto = document.querySelector(".popup_type_add-photo");
+const popupAddPhotoCloseButton = popupAddPhoto.querySelector(
+  ".popup__close-icon_type_form"
+);
+const popupFormAddPhoto = document.querySelector(".popup__form_type_add-photo");
+const titleInput = document.querySelector(".popup__input_type_title");
+const imageLinkInput = document.querySelector(".popup__input_type_image-link");
+const popupImage = document.querySelector(".popup_type_image");
+const popupImageCloseButton = document.querySelector(
+  ".popup__close-icon_type_image"
+);
+const popupBackground = popupImage.querySelector(".popup__background");
+const popupPlaceName = popupImage.querySelector(".popup__place-name");
+const cardTemplate = document.querySelector("#cardTemplate").content;
+
+function createCard(placeName, placePhoto) {
+  const card = cardTemplate.querySelector(".element").cloneNode(true);
+  const cardGarbageButton = card.querySelector(".element__garbage-button");
+  const cardImage = card.querySelector(".element__image");
+  const cardName = card.querySelector(".element__name");
+  const cardLikeButton = card.querySelector(".element__like-button");
+  cardName.textContent = placeName;
+  cardImage.alt = placeName;
+  cardImage.src = placePhoto;
+  cardGarbageButton.addEventListener("click", () => {
+    card.remove();
+  });
+  cardImage.addEventListener("click", () => {
+    addContentPopupImage(cardImage, placeName);
+    openPopup(popupImage);
+  });
+  cardLikeButton.addEventListener("click", () => {
+    cardLikeButton.classList.toggle("element__like-button_active");
+  });
+  return card;
+}
+
+function addContentPopupImage(cardImage, placeName) {
+  popupBackground.style.backgroundImage =
+    `
+      url(` +
+    cardImage.src +
+    `)`;
+  popupPlaceName.textContent = placeName;
+}
+
+function openPopup(popup) {
+  popup.classList.add("popup_active");
+}
+
+function openEditProfilePopup() {
+  nameInput.value = profileName.textContent;
+  aboutMeInput.value = profileAboutMe.textContent;
+  openPopup(popupEditProfile);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAboutMe.textContent = aboutMeInput.value;
+  closePopup(popupEditProfile);
+}
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  placesContainer.prepend(createCard(titleInput.value, imageLinkInput.value));
+  closePopup(popupAddPhoto);
+  popupFormAddPhoto.reset();
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_active");
+}
+
+initialCards.forEach((card) => {
+  placesContainer.append(createCard(card.name, card.link));
+});
+
+popupImageCloseButton.addEventListener("click", () => {
+  closePopup(popupImage);
+});
+editButton.addEventListener("click", openEditProfilePopup);
+popupFormEditProfile.addEventListener("submit", handleProfileFormSubmit);
+popupEditProfileCloseButton.addEventListener("click", () => {
+  closePopup(popupEditProfile);
+});
+addButton.addEventListener("click", () => {
+  openPopup(popupAddPhoto);
+});
+popupFormAddPhoto.addEventListener("submit", handleCardFormSubmit);
+popupAddPhotoCloseButton.addEventListener("click", () => {
+  closePopup(popupAddPhoto);
+});
